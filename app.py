11bae47d6ae14a40b0bf9c040f5107d697bd101f
Vaidpr <vaidpr98@gmail.com>
2025-04-17 03:16:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -673,8 +673,8 @@ def admin_work_log():
                 cursor.execute('SELECT Email, Name, Domain FROM ems WHERE Role != "Admin"')
             else:
                 # For HR, filter employees by domain
-                  hr_domain = current_user.domain  # Use the domain field directly 
-                   cursor.execute('SELECT Email, Name, Domain FROM ems WHERE Role = "Employee" AND Domain = %s',(hr_domain,))       
+               hr_domain = current_user.domain  # Use the domain field directly 
+               cursor.execute('SELECT Email, Name, Domain FROM ems WHERE Role = "Employee" AND Domain = %s',(hr_domain,))       
             employees = cursor.fetchall()
             print(f"Fetched employees: {employees}")  # Debug log
             
@@ -861,7 +861,7 @@ def assign_work():
             flash('Error assigning work', 'error')
             
     return redirect(url_for('admin_work_log'))
-    # return render_template("work_log.html", ..., user_role=current_user.role)
+
 
 
 @app.route('/delete-work/<int:work_id>')
